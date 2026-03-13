e31181fb02ea127c8d9cd989e5f6660068e92790
squalleon <spiritual.awaken@gmail.com>
2026-03-14 00:59:01 +0900

    3/14 tailwind.config.ts deleted tailwind.config.js added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
     extend: {},
@@ -8,5 +7,4 @@ const config: Config = {
   plugins: [],
 }
 
-export default config
-
+module.exports = config
